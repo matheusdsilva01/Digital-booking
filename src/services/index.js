c37b2011c675a625b5984ded5369ccdd15c11c16
Matheus Silva <ms25022003@gmail.com>
2022-05-29 18:00:55 -0300

    att api

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://ec2-3-223-123-204.compute-1.amazonaws.com:8080/api/'
+    baseURL: 'https://api-digital-booking-production.up.railway.app/api/'
 });
 
 export default api;
